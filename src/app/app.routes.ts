32dac02aed690840521a9042ad2e90edc91604c5
Alejandro osorio <engomadoonpixelesmail.com>
2025-05-29 16:53:02 -0500

    'checkout'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home'; // ← ajusta según tu ruta real
 import { Login } from './pages/login/login';
+import { Checkout } from './pages/checkout/checkout'; // Asegúrate de importar correctamente el componente Checkout
+import { authGuard } from './guards/auth.guard'; // Asegúrate de importar correctamente el guardia de autenticación
 export const routes: Routes = [
 
     { path: '', component: Home },
-    { path: 'login', component: Login }
+    { path: 'login', component: Login },
+    { path: 'checkout', component: Checkout, canActivate: [authGuard] }
+
 ];

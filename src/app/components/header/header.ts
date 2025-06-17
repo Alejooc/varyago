3068e07beddb1b303427dd7dba061520cc8db68f
Alejandro osorio <engomadoonpixeles@gmail.com>
2025-06-16 22:06:33 -0500

    'dsdsdf'

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute,RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../services/cart';
-
+import { HeaderService } from "../../services/header";
 @Component({
   selector: 'app-header',
   imports: [CommonModule,RouterModule],
@@ -12,15 +12,19 @@ import { CartService } from '../../services/cart';
 export class Header {
   cartCount = 0;
   cartItems: any[] = [];
+  menu: any ;
   constructor(
       private route: ActivatedRoute,
-      private cartService: CartService
+      private cartService: CartService,
+      private headerService: HeaderService
     ) {}
 
   ngOnInit(): void {
     this.cartCount = this.cartService.getTotalItems();
     this.cartItems = this.getCartItems();
     console.log(this.cartItems);
+    this.getMenu();
+    console.log(this.menu);
     
   }
   ngOnDestroy(): void {
@@ -33,4 +37,26 @@ export class Header {
   getCartItems() {
     return this.cartService.getCart();
   }
+  getMenu() {
+  this.headerService.getDepartments().subscribe(data => {
+    this.menu = data.map((item: any) => {
+     
+      return {
+        ...item,
+        submenuColumns: this.splitIntoColumns(item.submenu, 5)
+      };
+    });
+    console.log(this.menu);
+    
+  });
+}
+
+// Función para dividir en columnas
+splitIntoColumns(items: any[], itemsPerColumn: number = 5): any[][] {
+  const result = [];
+  for (let i = 0; i < items.length; i += itemsPerColumn) {
+    result.push(items.slice(i, i + itemsPerColumn));
+  }
+  return result;
+}
 }
